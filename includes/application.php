a85dc3f24f7dbaaf0689e92f1f0120464663693a
Binny V A <binnyva@gmail.com>
2019-02-21 15:49:48 +0530

    Updates

--- includes/application.php
+++ includes/application.php
@@ -3,7 +3,7 @@ $user_info = check_user();
 
 $user_id = $user_info['user_id'];
 $fam = new FAM;
-$year = 2018;
+// $year = 2017;
 
 // require dirname(__FILE__) . '/../../driller/models/Common.php';
 $common = new Common;
