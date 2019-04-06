bfcd897b5a2fa6bd86e9956f2e9944a66d296439
rhit99900 <rohitcse081@gmail.com>
2019-04-06 12:43:37 +0530

    Download Changes for DOCX

--- download.php
+++ download.php
@@ -4,7 +4,6 @@ $file_name = $_GET['file'];
 
 // make sure it's a file before doing anything!
 if(isset($file_name)) {
-
 	// get the file mime type using the file extension
 	switch(strtolower(substr(strrchr($file_name, '.'), 1))) {
 		case 'pdf': $mime = 'application/pdf'; header('location: '.$file_name); break;
@@ -15,14 +14,14 @@ if(isset($file_name)) {
 		case 'jpg': $mime = 'image/jpg'; header('location: '.$file_name); break;
 		default: $mime = 'application/force-download';
 	}
-	header('Pragma: public'); 	// required
-	header('Expires: 0');		// no cache
-	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-	header('Cache-Control: private',false);
-	header('Content-Type: '.$mime);
-	header('Content-Disposition: attachment; filename="'.basename($file_name).'"');
-	header('Content-Transfer-Encoding: binary');
-	header('Connection: close');
+	// header('Pragma: public'); 	// required
+	// header('Expires: 0');		// no cache
+	// header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+	// header('Cache-Control: private',false);
+	// header('Content-Type: '.$mime);
+	// header('Content-Disposition: attachment; filename="'.basename($file_name).'"');
+	// header('Content-Transfer-Encoding: binary');
+	// header('Connection: close');
 	readfile($file_name);		// push it out
 	exit();
 
