4e61792a3e7de8d09b787684931176decd4f43a9
rhit99900 <rohitcse081@gmail.com>
2019-04-06 12:26:24 +0530

    Download Changes for DOCX

--- download.php
+++ download.php
@@ -5,8 +5,6 @@ $file_name = $_GET['file'];
 // make sure it's a file before doing anything!
 if(isset($file_name)) {
 
-	if(ini_get('zlib.output_compression')) { ini_set('zlib.output_compression', 'Off');	}
-
 	// get the file mime type using the file extension
 	switch(strtolower(substr(strrchr($file_name, '.'), 1))) {
 		case 'pdf': $mime = 'application/pdf'; header('location: '.$file_name); break;
@@ -20,12 +18,10 @@ if(isset($file_name)) {
 	header('Pragma: public'); 	// required
 	header('Expires: 0');		// no cache
 	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-	header('Last-Modified: '.gmdate ('D, d M Y H:i:s', filemtime ($file_name)).' GMT');
 	header('Cache-Control: private',false);
 	header('Content-Type: '.$mime);
 	header('Content-Disposition: attachment; filename="'.basename($file_name).'"');
 	header('Content-Transfer-Encoding: binary');
-	header('Content-Length: '.filesize($file_name));	// provide file size
 	header('Connection: close');
 	readfile($file_name);		// push it out
 	exit();
