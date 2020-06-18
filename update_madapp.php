ff31e5b0a528dd79bf388241ebc3813a8d0f9dd2
rhit99900 <rohitcse081@gmail.com>
2020-06-18 14:16:51 +0530

    Email Update Fellows

--- update_madapp.php
+++ update_madapp.php
@@ -12,6 +12,23 @@ if(i($QUERY, 'approve') == "Yes") {
 $applications = [];
 $selected = [];
 
+$prevfellows = $sql->getAll("SELECT U.id as 'user_id'
+								FROM User U
+									INNER JOIN FAM_UserStage US ON US.user_id = U.id
+									INNER JOIN `Group` G ON G.id = US.group_id
+									INNER JOIN FAM_UserGroupPreference UGP ON UGP.user_id = U.id
+									INNER JOIN City C ON ((UGP.city_id != 0 AND UGP.city_id=C.id) OR (UGP.city_id = 0 AND U.city_id=C.id))
+								WHERE US.stage_id = 4 AND US.status = 'selected' AND US.year = ($year-1) and US.group_id <> 8
+								GROUP BY U.id
+								ORDER BY C.name ASC");
+
+foreach($prevfellows as $fellow_id){
+	$email_update = $sql->update('User',[
+		'mad_email' => NULL
+	],'id='.$fellow_id['user_id']);
+}
+
+// exit;
 
 $applications = $sql->getAll("SELECT U.id as 'user_id', U.name as 'name', C.name as 'city', G.name as 'role', G.id as 'group_id'
 								FROM User U
