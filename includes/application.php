1e4c2693bac353a98718e0b15dc0cd69ad2008de
Binny V A <Binny V A>
2021-04-18 18:29:23 +0530

    Updateing year

--- includes/application.php
+++ includes/application.php
@@ -1,8 +1,8 @@
 <?php
 $user_info = check_user();
 
-// $year = get_year();
-$year = 2019; //HardCoded;
+$year = get_year();
+// $year = 2019; //HardCoded;
 
 $user_id = $user_info['user_id'];
 $fam = new FAM;
@@ -26,8 +26,6 @@ foreach($user['groups'] as $grp) {
 	  break;
 	}
 }
-
-
 
 define('GROUP_ID_CTL', 2);
 define('GROUP_ID_ED', 19);
