ff85d6753a1863aa7656204a9dbbcee8651bcbf1
Binny V A <binnyva@gmail.com>
2021-08-26 15:11:33 +0530

    Updating fellow usergroup in DB based on selected fellows

--- templates/update_madapp.php
+++ templates/update_madapp.php
@@ -1,22 +1,19 @@
-<?php if($continue) { ?>
-    <div class="x_panel">
-      <div class="x_title">
-        <h2>Update On MADApp</h2>
-        <div class="clearfix"></div>
-      </div>
+<?php if(!$continue) { ?>
+<div class="x_panel">
+  <div class="x_title">
+    <h2>Update On MADApp</h2>
+    <div class="clearfix"></div>
+  </div>
 
-      <div class="x_content">
-        <p> Are you sure you want to update Fellows' <b>UserGroups</b> and <b>Email</b> on MADApp </p>
-        <form action="./update_madapp.php" method="POST">
-          <input type="submit" name="approve" value="Yes"/>
-          <input type="submit" name="approve" value="No"/>
-        </form>
-      </div>
-    </div>
-<?php
-  exit;
-}
-?>
+  <div class="x_content">
+    <p> Are you sure you want to update Fellows' <b>UserGroups</b> and <b>Email</b> on MADApp </p>
+    <form action="./update_madapp.php" method="POST">
+      <input type="submit" name="continue" value="Yes"/>
+      <input type="submit" name="continue" value="No"/>
+    </form>
+  </div>
+</div>
+<?php } else { ?>
 
 <div class="x_panel">
   <div class="x_title">
@@ -30,8 +27,6 @@
     <tr>
       <th>ID</th>
       <th>Fellow Name</th>
-      <!-- <th>Email</th> -->
-      <!-- <th>Sex</th> -->
       <th>City Name</th>
       <th>Role</th>
       <th>UserGroup Updated</th>
@@ -41,71 +36,14 @@
 
     <tbody>
       <?php
-        foreach ($applications as $app){
-
-          $ugp_status = '';
-          $email_status = '';
-
-
-          if(isset($email_ids[$app['user_id']])){
-            $email = $email_ids[$app['user_id']];
-          }
-          else{
-            $email = '';
-          }
-
-          $ugp_update_check = $sql->getOne('SELECT id FROM UserGroup UG WHERE UG.user_id='.$app['user_id'].' AND UG.group_id='.$app['group_id'].' AND UG.year='.$year);
-
-          if(empty($ugp_update_check)){
-            $ugp_add = $sql->insert('UserGroup',array(
-                'user_id'   => $app['user_id'],
-                'group_id'  => $app['group_id'],
-                'year'      => $year
-              ));
-
-            if($ugp_add>1){
-              $ugp_status = 'Saved';
-            }
-            else{
-              $ugp_status = 'Failed';
-            }
-          }
-          else{
-            $ugp_status = 'Updated';
-          }
-
-          $email_update_check = $sql->getOne('SELECT id FROM User U WHERE U.id='.$app['user_id'].' AND U.mad_email="'.$email.'"');
-
-          if(empty($email_update_check) && $email!=''){
-            $mad_email_add = $sql->update('User',array(
-              'mad_email' => $email
-            ),'id='.$app['user_id']);
-
-            if($mad_email_add>=1){
-              $email_status = 'Saved';
-            }
-            else{
-              $email_status = 'Failed';
-            }
-          }
-          else{
-            $email_status = 'Updated';
-          }
-
-          if($email==''){
-            $email_status = 'Not Found';
-          }
-
-
+        foreach ($selected as $app){
           echo '<tr>'.
                 '<td>'.$app['user_id'].'</td>'.
                 '<td>'.$app['name'].'</td>'.
-                // '<td>'.$app['email'].'</td>'.
-                // '<td>'.$app['sex'].'</td>'.
                 '<td>'.$app['city'].'</td>'.
                 '<td>'.$app['role'].'</td>'.
-                '<td><b>'.$ugp_status.'</b></td>'.
-                '<td><b>'.$email_status.'</b> <em>'.$email.'</em></td>'.
+                '<td><b>'.$group_updated[$app['user_id']].'</b></td>'.
+                '<td><b>'.$email_updated[$app['user_id']].'</b></td>'.
               '</tr>';
         }
       ?>
@@ -113,12 +51,4 @@
   </table>
   </div>
 </div>
-
-<?php
-
-function highlight($applications, $requirements) {
-    global $multiplication_factor;
-
-    if($applications < $requirements * $multiplication_factor) echo ' class="error-message"';
-    else echo ' class="success-message"';
-}
+<?php } ?>
--- update_madapp.php
+++ update_madapp.php
@@ -1,82 +1,72 @@
 <?php
 require 'common.php';
 
+$continue = (i($QUERY, 'continue', 'No') === 'Yes') ? true : false;
+
+if(!$continue) {
+	render();
+	exit;
+}
+
+// Asumption : Script will be run in the new year. 
 $all_cities = keyFormat($common->getCities(), ['id', 'name']);
 
-if(i($QUERY, 'approve') == "Yes") {
-	$continue = false;
-} else{
-	$continue = true;
-}
-
-$applications = [];
 $selected = [];
 
-$prevfellows = $sql->getAll("SELECT U.id as 'user_id'
+// First, unset all last year fellow's ka email id.
+$last_year = $year - 1;
+$prev_fellows = $sql->getAll("SELECT U.id as 'user_id', G.name
+								FROM User U
+								INNER JOIN `UserGroup` UG ON U.id = UG.user_id AND UG.year = $last_year
+								INNER JOIN `Group` G ON G.id = UG.group_id
+								WHERE G.type = 'fellow' AND G.group_type = 'normal' AND G.status='1' AND U.mad_email IS NOT NULL"); // :TODO: Strats?
+
+foreach($prev_fellows as $fellow_id){
+	$sql->update('User',[ 'mad_email' => NULL ], 'id='.$fellow_id['user_id']);
+}
+
+$mentor_group_id = 8;
+$selected = $sql->getAll("SELECT U.id as 'user_id', U.name as 'name', C.name as 'city', G.name as 'role', G.id as 'group_id'
 								FROM User U
 									INNER JOIN FAM_UserStage US ON US.user_id = U.id
 									INNER JOIN `Group` G ON G.id = US.group_id
 									INNER JOIN FAM_UserGroupPreference UGP ON UGP.user_id = U.id
 									INNER JOIN City C ON ((UGP.city_id != 0 AND UGP.city_id=C.id) OR (UGP.city_id = 0 AND U.city_id=C.id))
-								WHERE US.stage_id = 4 AND US.status = 'selected' AND US.year = ($year-1) and US.group_id <> 8
+								WHERE US.stage_id = 4 AND US.status = 'selected' AND US.year = $last_year AND US.group_id != $mentor_group_id
 								GROUP BY U.id
 								ORDER BY C.name ASC");
 
-foreach($prevfellows as $fellow_id){
-	$email_update = $sql->update('User',[
-		'mad_email' => NULL
-	],'id='.$fellow_id['user_id']);
-}
+// This part sets up the new emails of the new fellows. REQUIRES A sheet with the user id => email mapping.
+// $email_ids = getEmailFromSheet(
+// 				"https://docs.google.com/spreadsheets/d/e/2PACX-1vTQE8sKcse5jEOAhOC5onBvLJxjIlFWC8n66Ced9WMte5uKjsUcVHs-N3j1aNZW_Uj0VupbNsQRFjHC/pub?gid=1773549663&single=true&output=csv",
+// 				'A','F');
+// foreach ($selected as $key => $fellow) {
+// 	$selected[$key]['new_email'] = $email_ids[$fellow['user_id']];
+// }
 
-// exit;
+$group_updated = [];
+$email_updated = [];
 
-$applications = $sql->getAll("SELECT U.id as 'user_id', U.name as 'name', C.name as 'city', G.name as 'role', G.id as 'group_id'
-								FROM User U
-									INNER JOIN FAM_UserStage US ON US.user_id = U.id
-									INNER JOIN `Group` G ON G.id = US.group_id
-									INNER JOIN FAM_UserGroupPreference UGP ON UGP.user_id = U.id
-									INNER JOIN City C ON ((UGP.city_id != 0 AND UGP.city_id=C.id) OR (UGP.city_id = 0 AND U.city_id=C.id))
-								WHERE US.stage_id = 4 AND US.status = 'selected' AND US.year = $year and US.group_id <> 8
-								GROUP BY U.id
-								ORDER BY C.name ASC");
+foreach ($selected as $fellow) {
+	$group_updated[$fellow['user_id']] = 'Failed';
+	$exsiting = $sql->getOne("SELECT id FROM UserGroup WHERE user_id=".$fellow['user_id']." AND group_id=".$fellow['group_id']." AND year = $year");
+	if(!$exsiting) {
+		$sql->insert("UserGroup", [
+			'user_id'	=> $fellow['user_id'],
+			'group_id'	=> $fellow['group_id'],
+			'year'		=> $year
+		]);
+		$group_updated[$fellow['user_id']] = 'Updated';
+	} else {
+		// echo "User $fellow[user_id] Set already. No Updated necessary.<br />";
+		$group_updated[$fellow['user_id']] = 'Not Needed';
+	}
 
-
-$email_ids = getEmailFromSheet("https://docs.google.com/spreadsheets/d/e/2PACX-1vTQE8sKcse5jEOAhOC5onBvLJxjIlFWC8n66Ced9WMte5uKjsUcVHs-N3j1aNZW_Uj0VupbNsQRFjHC/pub?gid=1773549663&single=true&output=csv",'A','F');
-
-
-// dump($applications);
-// dump($email_ids);
-// exit;
-
-foreach ($applications as $key => $fellows) {
-	$applications[$key]['new_email'] = $email_ids[$fellows['user_id']];
-}
-
-if($continue == false){
-	$new_year = ++$year;
-	// dump($new_year);exit;
-
-	clear_current_email($sql);
-
-	foreach ($applications as $fellows) {
-		// echo "<script>console.log('fellow_user_id:".$fellows['user_id']."; group_id:".$fellows['group_id']."');</script>";
-		$exsiting = $sql->getOne("SELECT id FROM UserGroup WHERE user_id=".$fellows['user_id']." AND group_id=".$fellows['group_id']." AND year=".$new_year."");
-		if(!$exsiting) {
-			$sql->insert("UserGroup", [
-				'user_id'	=> $fellows['user_id'],
-				'group_id'	=> $fellows['group_id'],
-				'year'		=> $new_year
-			]);
-			// echo "<script>console.log('Updated');</script>";
-		} else {
-			// echo "<script>console.log('Exists Already');</script>";
-		}
-		$email_update = $sql->update('User',[
-			'mad_email' => $fellows['new_email']
-		],'id='.$fellows['user_id']);
-		// echo "<script>console.log('Email Updated');</script>";
+	$email_updated[$fellow['user_id']] = 'Not Updated';
+	if(isset($fellow['new_email'])) {
+		$sql->update('User',[ 'mad_email' => $fellow['new_email'] ],'id='.$fellow['user_id']);
+		$email_updated[$fellow['user_id']] = 'Updated';
 	}
 }
 
-$multiplication_factor = 3;
 render();
